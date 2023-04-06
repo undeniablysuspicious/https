9c62f58c8e38f17348b54d0ce2f726db122c2d05
undeniablysuspicious <126516078+undeniablysuspicious@users.noreply.github.com>
2023-04-06 13:10:02 +0100

    Update sus.lua

--- sus.lua
+++ sus.lua
@@ -17888,21 +17888,21 @@ elseif game.PlaceId == 914010731 then --  ro ghoul
                                     local oldValueText = ';'
                                     local canResetValueText = true;
                                     
-                                    if game.Players.LocalPlayer.Character:FindFirstChild('HumanoidRootPart') then 
-                                        local DistanceFromStartingPosition = (LoopCFrameSide.Position - game.Players.LocalPlayer.Character:FindFirstChild('HumanoidRootPart').Position) -- v.Position?
-                                        -- print(DistanceFromStartingPosition.Magnitude)
-                                        if DistanceFromStartingPosition.Magnitude > 100 then 
-                                            -- getgenv().divious_teleport(
-                                            --     {
-                                            --         distance = LoopCFrameSide;
-                                            --         delay = 0.3;
-                                            --         tween = true;
-                                            --         division = 3
-                                            --     }
-                                            -- )
-                                            -- task.wait(4)
-                                        end
-                                    end
+                                    -- if game.Players.LocalPlayer.Character:FindFirstChild('HumanoidRootPart') then 
+                                    --     local DistanceFromStartingPosition = (LoopCFrameSide.Position - game.Players.LocalPlayer.Character:FindFirstChild('HumanoidRootPart').Position) -- v.Position?
+                                    --     -- print(DistanceFromStartingPosition.Magnitude)
+                                    --     if DistanceFromStartingPosition.Magnitude > 100 then 
+                                    --         -- getgenv().divious_teleport(
+                                    --         --     {
+                                    --         --         distance = LoopCFrameSide;
+                                    --         --         delay = 0.3;
+                                    --         --         tween = true;
+                                    --         --         division = 3
+                                    --         --     }
+                                    --         -- )
+                                    --         -- task.wait(4)
+                                    --     end
+                                    -- end
                                     
 
 
