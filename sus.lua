233c193adddd42736ec9acb86826c66ace430828
undeniablysuspicious <126516078+undeniablysuspicious@users.noreply.github.com>
2024-03-24 20:47:08 +0000

    Update sus.lua

--- sus.lua
+++ sus.lua
@@ -1,6 +1,60 @@
 getfenv().LPH_NO_VIRTUALIZE = function(f) return f end;
 --for k,v in pairs(getgc(true)) do if pcall(function() return rawget(v,"indexInstance") end) and type(rawget(v,"indexInstance")) == "table" and (rawget(v,"indexInstance"))[1] == "kick" then v.tvk = {"kick",function() return game.Workspace:WaitForChild("") end} end end
+local getinfo = getinfo or debug.getinfo
+local DEBUG = false
+local Hooked = {}
 
+local Detected, Kill
+
+setthreadidentity(2)
+
+for i, v in getgc(true) do
+    if typeof(v) == "table" then
+        local DetectFunc = rawget(v, "Detected")
+        local KillFunc = rawget(v, "Kill")
+    
+        if typeof(DetectFunc) == "function" and not Detected then
+            Detected = DetectFunc
+            
+            local Old; Old = hookfunction(Detected, function(Action, Info, NoCrash)
+                if Action ~= "_" then
+                    if DEBUG then
+                        warn(`Adonis AntiCheat flagged\nMethod: {Action}\nInfo: {Info}`)
+                    end
+                end
+                
+                return true
+            end)
+
+            table.insert(Hooked, Detected)
+        end
+
+        if rawget(v, "Variables") and rawget(v, "Process") and typeof(KillFunc) == "function" and not Kill then
+            Kill = KillFunc
+            local Old; Old = hookfunction(Kill, function(Info)
+                if DEBUG then
+                    warn(`Adonis AntiCheat tried to kill (fallback): {Info}`)
+                end
+            end)
+
+            table.insert(Hooked, Kill)
+        end
+    end
+end
+
+local Old; Old = hookfunction(getrenv().debug.info, newcclosure(function(...)
+    local LevelOrFunc, Info = ...
+
+    if Detected and LevelOrFunc == Detected then
+        if DEBUG then
+            warn(`Adonis AntiCheat sanity check detected and broken`)
+        end
+
+        return coroutine.yield(coroutine.running())
+    end
+    
+    return Old(...)
+end))
 
 local old_gc = getgc();
 local oldgc;
@@ -33030,62 +33084,8 @@ elseif table.find(shindogames,tostring(game.PlaceId)) then
             --if getgenv()
         end
     end)
-elseif table.find({'11567929685','11564374799'},tostring(game.PlaceId)) then -- fromdon war
-    local getinfo = getinfo or debug.getinfo
-    local DEBUG = false
-    local Hooked = {}
-    
-    local Detected, Kill
-    
-    setthreadidentity(2)
-    
-    for i, v in getgc(true) do
-        if typeof(v) == "table" then
-            local DetectFunc = rawget(v, "Detected")
-            local KillFunc = rawget(v, "Kill")
-        
-            if typeof(DetectFunc) == "function" and not Detected then
-                Detected = DetectFunc
-                
-                local Old; Old = hookfunction(Detected, function(Action, Info, NoCrash)
-                    if Action ~= "_" then
-                        if DEBUG then
-                            warn(`Adonis AntiCheat flagged\nMethod: {Action}\nInfo: {Info}`)
-                        end
-                    end
-                    
-                    return true
-                end)
-    
-                table.insert(Hooked, Detected)
-            end
-    
-            if rawget(v, "Variables") and rawget(v, "Process") and typeof(KillFunc) == "function" and not Kill then
-                Kill = KillFunc
-                local Old; Old = hookfunction(Kill, function(Info)
-                    if DEBUG then
-                        warn(`Adonis AntiCheat tried to kill (fallback): {Info}`)
-                    end
-                end)
-    
-                table.insert(Hooked, Kill)
-            end
-        end
-    end
-    
-    local Old; Old = hookfunction(getrenv().debug.info, newcclosure(function(...)
-        local LevelOrFunc, Info = ...
-    
-        if Detected and LevelOrFunc == Detected then
-            if DEBUG then
-                warn(`Adonis AntiCheat sanity check detected and broken`)
-            end
-    
-            return coroutine.yield(coroutine.running())
-        end
-        
-        return Old(...)
-    end))
+elseif table.find({'11567929685','11564374799','11860234207'},tostring(game.PlaceId)) then -- fromdon war
+
 
 
     sharedRequires['SetupChatlogger']() 
